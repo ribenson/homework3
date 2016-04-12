664a189aa8fdf59b08d521e84917206298a724c5
Riley <ribenson@wisc.edu>
2016-04-11 21:03:38 -0500

    element_lib module now has function to grab chemical symbol of a line and a test for it

--- elelib_tests.py
+++ elelib_tests.py
@@ -1,10 +1,17 @@
 from nose.tools import assert_equal
 from element_lib import parse_first_line as pfl
+from element_lib import grab_chem_symbol
 
 def test_pfl_1():
     file_name = 'elelib.std'
     obs = pfl(file_name)
     exp = [83, 368]
+    assert_equal(obs[0:2],exp)
+
+def test_chem_sym():
+    input_file = open('element_sample.std')
+    for line in input_file:
+        obs = grab_chem_symbol(line)
+        return obs
+    exp = 'h'
     assert_equal(obs,exp)
-
-
--- element_lib.py
+++ element_lib.py
@@ -1,4 +1,13 @@
 # Module to parse and read an element library into a single data structure
+def grab_chem_symbol(line):
+    if line[1] == ' ':
+        chem_sym = line[0]
+    elif line[1] != ' ':
+        chem_sym = line[0,2]
+    return chem_sym
+
+def grab_molar_mass(line):
+    pass
 
 
 def parse_first_line(file_name):
@@ -9,7 +18,6 @@ def parse_first_line(file_name):
 
     for line in input_file:
         if re.match("[a-z]",line[0]):
-            
             element_cnt = element_cnt + 1
         line_count = line_count + 1
     
