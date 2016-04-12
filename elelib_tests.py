3bd52780cc30a4c49c6e3ec3498ce06ac42d6e9a
Riley <ribenson@wisc.edu>
2016-04-11 21:22:42 -0500

    element_lib has functions to parse all info from first line of each block and each is tested to work on hydrogen

--- elelib_tests.py
+++ elelib_tests.py
@@ -1,7 +1,7 @@
 from nose.tools import assert_equal
 from element_lib import parse_first_line as pfl
-from element_lib import grab_chem_symbol
-
+from element_lib import grab_chem_symbol, grab_molar_mass, grab_z_number
+from element_lib import grab_density, grab_iso_number
 def test_pfl_1():
     file_name = 'elelib.std'
     obs = pfl(file_name)
@@ -15,3 +15,32 @@ def test_chem_sym():
         return obs
     exp = 'h'
     assert_equal(obs,exp)
+
+def test_molar_mass():
+    input_file = open('element_sample.std')
+    for line in input_file:
+        obs = grab_molar_mass(line)
+        return obs
+    exp = '0.100790E+01'
+    assert_equal(obs,exp)
+
+def test_z_number():
+    input_file = open('element_sample.std')
+    for line in input_file:
+        obs = grab_z_number(line)
+        return obs
+    exp = '1'
+
+def test_density():
+    input_file = open('element_sample.std')
+    for line in input_file:
+        obs = grab_density(line)
+        return obs
+    exp = '0.899000E-04'
+
+def test_iso_number():
+    input_file = open('element_sample.std')
+    for line in input_file:
+        obs = grab_iso_number(line)
+        return obs
+    exp = '2'
--- element_lib.py
+++ element_lib.py
@@ -1,4 +1,8 @@
 # Module to parse and read an element library into a single data structure
+
+#=============================================================
+# Group of Functions Used to Parse First Line of Each Block
+#=============================================================
 def grab_chem_symbol(line):
     if line[1] == ' ':
         chem_sym = line[0]
@@ -7,8 +11,27 @@ def grab_chem_symbol(line):
     return chem_sym
 
 def grab_molar_mass(line):
-    pass
+    molar_mass = line[8:20]
+    return molar_mass
 
+def grab_z_number(line):
+    element_z = line[22:24]
+    element_z = element_z.strip
+    return element_z
+
+def grab_density(line):
+    density_STP = line[30:42]
+    return density_STP
+
+def grab_iso_number(line):
+    line_len = len(line.strip())
+    iso_number = line[line_len-2:line_len]
+    return iso_number
+
+#============================================================
+# Function that parses first line of each file and puts it in
+# an element library dictionary
+#============================================================
 
 def parse_first_line(file_name):
     import re
