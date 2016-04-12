80185d30efdc5bf436a51cfadc8d528a57878eac
Riley <ribenson@wisc.edu>
2016-04-12 02:28:01 -0500

    Completed module that can run through elelib.std and create element library

--- element_lib.py
+++ element_lib.py
@@ -4,10 +4,8 @@
 # Group of Functions Used to Parse First Line of Each Block
 #=============================================================
 def grab_chem_symbol(line):
-    if line[1] == ' ':
-        chem_sym = line[0]
-    elif line[1] != ' ':
-        chem_sym = line[0,2]
+    chem_sym = line[0:2]
+    chem_sym = chem_sym.strip()
     return chem_sym
 
 def grab_molar_mass(line):
@@ -16,34 +14,73 @@ def grab_molar_mass(line):
 
 def grab_z_number(line):
     element_z = line[22:24]
-    element_z = element_z.strip
+    element_z = element_z.strip()
     return element_z
 
 def grab_density(line):
     density_STP = line[30:42]
     return density_STP
 
-def grab_iso_number(line):
+def grab_num_of_iso(line):
     line_len = len(line.strip())
-    iso_number = line[line_len-2:line_len]
-    return iso_number
+    num_of_iso = line[line_len-1:line_len]
+    return num_of_iso
 
-#============================================================
-# Function that parses first line of each file and puts it in
-# an element library dictionary
-#============================================================
+#===================================================================
+# Function that parses the first line of a block
+#===================================================================
 
-def parse_first_line(file_name):
-    import re
-    input_file = open(file_name)
-    element_cnt = 0
-    line_count = 0
-
-    for line in input_file:
-        if re.match("[a-z]",line[0]):
-            element_cnt = element_cnt + 1
-        line_count = line_count + 1
+def parse_first_line(line):
+    chem_sym = grab_chem_symbol(line)
+    molar_mass = grab_molar_mass(line)
+    element_z = grab_z_number(line)
+    density_STP = grab_density(line)
+    num_of_iso = grab_num_of_iso(line)
     
-    return [element_cnt, line_count]
+    return [chem_sym, molar_mass, element_z, density_STP, num_of_iso]
 
+#===================================================================
+# Function that parses the isotopic abundance information from rest
+# of the block.
+#===================================================================
+
+def parse_isotopic_abund(line):
+    line = line.strip()
+    iso_mass_num = line[0:3]
+    iso_mass_num = iso_mass_num.strip()
+
+    iso_abund = line[-12:]
+
+    return [iso_mass_num, iso_abund]
+
+
+
+#======================================================================
+# Function to open the file and loop over all the lines creating 
+# the element library by calling the above functions.
+#======================================================================
+
+def elem_lib_from_file(filename, input_dict):
+    import re
+
+    for line in filename:
+        if re.match("[a-z]",line[0]):
+            isotope_data = {}
+            element_info = parse_first_line(line)
+
+            chem_sym = element_info[0]
+            molar_mass = element_info[1]
+            element_z = element_info[2]
+            density_STP = element_info[3]
+            num_of_iso = element_info[4]
+
+        elif line[0] == ' ':
+            abund_info = parse_isotopic_abund(line)
+            ZZZAAA_id = int(element_z)*1000+int(abund_info[0])
+
+            isotope_data[ZZZAAA_id] = abund_info[1]
+
+        input_dict[chem_sym] = {'molar mass':molar_mass, 'Z':element_z, 'density':density_STP, 'number of natural isotopes':num_of_iso, 'isotope abundance':isotope_data}
+
+    return input_dict
 
